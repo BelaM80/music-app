547a68257085ec8aacc4a83f9310ed1eddae6c31
BelaM80 <mueller.bela@gmx.de>
2020-10-06 15:46:29 +0200

    Add sharbutton

--- src/components/btn/btn.stories.js
+++ src/components/btn/btn.stories.js
@@ -1,6 +1,7 @@
 import "./btn.css";
 import buttonLogo from "./play-btn-logo.html";
-
+import buttonShare from "./share-btn-logo.html";
 export default { title: "Components/Btn" };
 
 export const withLogo = () => buttonLogo;
+export const withShare = () => buttonShare;
