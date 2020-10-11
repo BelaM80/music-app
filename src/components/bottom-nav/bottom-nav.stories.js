d47533cb5955e7fcb5bb6c36ecca269a19fb2411
BelaM80 <mueller.bela@gmx.de>
2020-10-11 12:52:54 +0200

    Add missing active icons

--- src/components/bottom-nav/bottom-nav.stories.js
+++ src/components/bottom-nav/bottom-nav.stories.js
@@ -1,6 +1,10 @@
 import "./bottom-nav.css";
 import bottomNav from "./bottom-nav.html";
+import bottomNav2 from "./bottom-nav2.html";
+import bottomNav3 from "./bottom-nav3.html";
 
 export default { title: "Components/Bottom-Nav" };
 
 export const navBottom = () => bottomNav;
+export const navBottom2 = () => bottomNav2;
+export const navBottom3 = () => bottomNav3;
